bf21ab628aec8d1c03c59f866117a084d06b0726
RelancioBorgesDev <relancio.dev@gmail.com>
2022-12-30 21:11:27 -0300

    Paginas inicias do projeto & document

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "../styles/global/themes/default";
+import { GlobalStyles } from "../styles/global/globals";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
