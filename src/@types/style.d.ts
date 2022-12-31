69b23580ee21108bf77359ba7cce252ec4a1b889
RelancioBorgesDev <relancio.dev@gmail.com>
2022-12-30 21:10:03 -0300

    Tipagem do tema

---
+++ src/@types/style.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+import { defaultTheme } from "../styles/global/themes/default";
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
