1713f3b6c4c841484430d11026e7d308d2e2d6ec
RelancioBorgesDev <relancio.dev@gmail.com>
2022-12-30 21:09:27 -0300

    Estilo global & tema da aplicacao

---
+++ src/styles/global/globals.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles =  createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        overflow: hidden;
+    }
+
+    body {
+	    line-height: 1;
+        font-size: ${({theme}) => theme.fontSizes.md};
+        color: ${({theme}) => theme.colors.platinum_text};
+        font-weight: ${({theme}) => theme.fontWeights.normal};
+        background-color: ${({theme}) => theme.colors.pink_bg};
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+    
+
+`
---
+++ src/styles/global/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+    "colors": {
+        light_red: "#DD1C1A",
+        dark_red: "#8A3033",
+        orange: "#EF7B45",
+        dodger_blue: "#2191FB",
+        platinum_text: "#E5E5E5",
+        pink_bg: "#E8D6CB" ,
+    },
+    "fontSizes": {
+        tn: "0.5rem",
+        sm: "0.75rem",
+        md: "1rem",
+        lg: "1.5rem",
+        xl: "2.375rem",
+    },
+    "fontWeights": {
+        normal: "400",
+        bold: "700",
+    }
+} as const
